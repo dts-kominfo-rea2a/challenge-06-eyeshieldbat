04777bb33d4922f4107b099c265b01bf643214e7
2nhard <eyeshield.bat@gmail.com>
2022-06-12 15:33:20 +0700

    Challenge 06 - Renhard

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -16,9 +17,43 @@ let modifyFile3 = (val) => {
   file3 = val;
 };
 
+
+
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnReadData) => {
+  let result = [];
+
+  // json 1
+  fs.readFile(file1, "utf8", (err, data) => {
+    if (err) {
+      return fnReadData(err, null);
+    }
+    const resultData = JSON.parse(data);
+    result.push(resultData.message.split(" ")[1]);
+
+    // json 2
+    fs.readFile(file2,"utf8", (err, data) => {
+      if (err) {
+        return fnReadData(err, null);
+      }
+      const resultData = JSON.parse(data);
+      result.push(resultData[0].message.split(" ")[1]);
+
+      // json 3
+      fs.readFile(file3, "utf8", (err, data) => {
+        if (err) {
+          return fnReadData(err, null);
+        }
+        const resultData = JSON.parse(data);
+        result.push(resultData[0].data.message.split(" ")[1]);
+        fnReadData(null, result);
+
+      });
+    });
+  })
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
